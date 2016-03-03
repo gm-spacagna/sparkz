07cbe194fffcf502f36e1ea088cde89c2e04452c
Spacagna, Gianmario : Personal & Corporate Banking <Gianmario.Spacagna@barclayscorp.com>
2016-03-03 16:55:22 +0000

    MAP

---
+++ src/main/scala/sparkz/evaluation/MAP.scala
@@ -0,0 +1,19 @@
+package sparkz.evaluation
+
+import org.apache.spark.rdd.RDD
+
+import scalaz.Scalaz._
+
+object MAP {
+  def apply[T](n: Int = 100, recommendations: RDD[(Long, List[T])], evaluation: RDD[(Long, Set[T])]): Double =
+    recommendations.join(evaluation).values.map {
+      case (recommendedLikes, trueLikes) => recommendedLikes.take(n).zipWithIndex.foldLeft(0, 0.0) {
+        case ((accLikes, accPrecision), (postId, k)) if trueLikes(postId) =>
+          (accLikes + 1, accPrecision + ((accLikes + 1).toDouble / (k + 1)))
+        case ((accLikes, accPrecision), _) => (accLikes, accPrecision)
+      }._2 / math.min(trueLikes.size, n)
+    } |> (apn => {
+      val count = apn.count()
+      if (count > 0) apn.reduce(_ + _) / count else 0
+    })
+}
